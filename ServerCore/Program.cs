ef1e946485b1a5000ccf3e98418d17386bc72d99
정호진 <dnjfs@naver.com>
2023-04-01 15:39:58 +0900

    DeadLock
    데드락 예제
    호출 타이밍을 조절하여 데드락을 방지하거나, Id를 할당 후 사이클을 확인하여 데드락 수정

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -4,6 +4,48 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,38 +53,17 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                // 상호배제 Mutual Exclusive
-                lock (_obj)
-                {
-                    number++;
-                }
-
-                /*
-                try
-                {
-                    Monitor.Enter(_obj); // 문을 잠구는 행위
-                    number++;
-
-                    return;
-                }
-                finally
-                {
-                    Monitor.Exit(_obj); // 잠금을 풀어준다
-                }
-                */
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 1000000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
@@ -51,6 +72,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(100);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
