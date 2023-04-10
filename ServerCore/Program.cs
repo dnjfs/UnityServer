80989e259ab6763aa6e58bfe1774fa3ce44281e7
정호진 <dnjfs@naver.com>
2023-04-10 22:04:06 +0900

    ReaderWriterLock
    읽을 땐 ReadLock, 쓸 땐 WriteLock
    WriteLock이 걸려있지 않는 한 ReadLock엔 여러 쓰레드가 들어올 수 있음

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,39 +6,46 @@ namespace ServerCore
 {
     class Program
     {
-        static int _num = 0;
-        static Mutex _lock = new Mutex();
+        // 1. 근성
+        // 2. 양보
+        // 3. 갑질
 
-        static void Thread_1()
+        static object _lock = new object();
+        static SpinLock _lock2 = new SpinLock();
+        static ReaderWriterLockSlim _lock3 = new ReaderWriterLockSlim();
+
+        // 직접 만든다
+
+        // [ ] [ ] [ ] [ ] [ ]
+        class Reward
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num++;
-                _lock.ReleaseMutex();
-            }
+
         }
 
-        static void Thread_2()
+        // 99.999999%
+        static Reward GetRewardById(int id)
         {
-            for (int i = 0; i < 100000; i++)
-            {
-                _lock.WaitOne();
-                _num--;
-                _lock.ReleaseMutex();
-            }
+            _lock3.EnterReadLock();
+
+            _lock3.ExitReadLock();
+
+            return null;
+        }
+
+        // 0.000001%
+        static void AddReward(Reward reward)
+        {
+            _lock3.EnterWriteLock();
+
+            _lock3.ExitWriteLock();
         }
 
         static void Main(string[] args)
         {
-            Task t1 = new Task(Thread_1);
-            Task t2 = new Task(Thread_2);
-            t1.Start();
-            t2.Start();
+            lock (_lock)
+            {
 
-            Task.WaitAll(t1, t2);
-
-            Console.WriteLine(_num);
+            }
         }
     }
 }
