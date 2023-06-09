334a67cc034ffc2005b83859828a35c9fe6ad01a
정호진 <dnjfs@naver.com>
2023-06-09 13:11:01 +0900

    Session #1
    Receive, Send 별도 코드로 분리
    Disconnection 연속으로 했을 시 처리

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -28,8 +28,11 @@ namespace DummyClient
                     Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
 
                     // 보낸다
-                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
-                    int sendBytes = socket.Send(sendBuff);
+                    for (int i = 0; i < 5; i++)
+                    {
+                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                        int sendBytes = socket.Send(sendBuff);
+                    }
 
                     // 받는다
                     byte[] recvBuff = new byte[1024];
@@ -46,7 +49,7 @@ namespace DummyClient
                     Console.WriteLine(e.ToString());
                 }
 
-                Thread.Sleep(1000);
+                Thread.Sleep(100);
             }
         }
     }
